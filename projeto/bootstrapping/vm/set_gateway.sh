7def1be51ebb063e75a40b16f845e026508dc0a3
bernardocarrilho <51291636+bernardocarrilho@users.noreply.github.com>
2022-12-07 18:36:02 +0000

    Update set_gateway.sh

--- projeto/bootstrapping/vm/set_gateway.sh
+++ projeto/bootstrapping/vm/set_gateway.sh
@@ -5,10 +5,6 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 192.168.88.101 -o eth0 -j MASQUERADE
 sudo iptables -t nat -A POSTROUTING -s 192.168.88.102 -o eth0 -j MASQUERADE
 
-# Setup VmB Gateway
-ssh vmB sudo ip route del default
-ssh vmB sudo ip route add default via 192.168.88.100
-
 # Setup VmC Gateway
 ssh vmC sudo ip route del default
 ssh vmC sudo ip route add default via 192.168.88.100
